ce50865b849b055da05653d9f3183059186743b1
Uwancha <sito2alemu@gmail.com>
2024-03-25 22:08:04 +0300

    feat: Add authentication middleware
    Implemented authentication middleware for verifying JWT tokens in the authenticateMiddleware.ts file.
    
    - Added middleware function VerifyToken to verify JWT tokens extracted from headers.
    - The middleware verifies the token using the JWT secret and attaches the decoded user information to the request object.
    - If the token is invalid or expired, it returns a 403 Forbidden response.

---
+++ src/middlewares/authenticateMiddleware.ts
@@ -0,0 +1,38 @@
+import Jwt, { JwtPayload } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+// Define a custom interface that extends express Request
+export interface CustomRequest extends Request {
+    user?: string  | JwtPayload;
+}
+
+/**
+ * Verifies JWT token.
+ * @param {CustomRequest} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Response} An HTTP response indicating success or failure.
+ */
+export const VerifyToken = (req: CustomRequest, res: Response, next: NextFunction) => {
+    // Extract token from headers or cookies
+    const token = req.headers.authorization?.split(' ')[1];
+
+    // Ensure that the token exists
+    if (!token) {
+        return res.status(401).json({ message: "Authorization token is required" });
+    };
+
+    try {
+        // Verify token using JWT secret
+        const jwtSecret = process.env.JWTSECRET as string;
+        const decoded :string | Jwt.JwtPayload = Jwt.verify(token, jwtSecret);
+
+        // Attach decoded user information to request object
+        req.user = decoded;
+
+        // Call next middleware
+        return next();
+    } catch (error) {
+        return res.status(403).json({ message: "Invalid or expired token" });
+    };
+};
