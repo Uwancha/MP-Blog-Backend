4b56f895ca5b4288f5442902004f90e758901938
Uwancha <sito2alemu@gmail.com>
2024-03-25 22:14:08 +0300

    feat: Add function to upload avatar to Cloudinary
    Added a function to upload avatar images to Cloudinary in the uploadAvatarToCloudinary.ts file.
    
    - Implemented a function to upload an avatar image to Cloudinary using the Cloudinary SDK.
    - The function takes the path of the avatar image as input and returns a Promise that resolves with the Cloudinary response object upon successful upload.
    - If an error occurs during the upload process, an informative error message is thrown.
    
    This function facilitates the uploading of avatar images to Cloudinary for user profiles.

---
+++ src/utils/cloudinaryConfig.ts
@@ -0,0 +1,15 @@
+import { v2 as cloudinaryV2} from 'cloudinary';
+
+/**
+ * Configure Cloudinary SDK with the provided credentials.
+ * @param {string} cloud_name - Cloudinary cloud name.
+ * @param {string} api_key - Cloudinary API key.
+ * @param {string} api_secret - Cloudinary API secret.
+ */
+cloudinaryV2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET, 
+});
+
+export { cloudinaryV2 };
