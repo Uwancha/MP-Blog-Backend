6faf711ec3b155ae11653ad71e18a48f5ddb4ffa
Uwancha <sito2alemu@gmail.com>
2024-03-25 22:03:37 +0300

    feat: Add post controllers
    Added controllers for managing posts in the postControllers.ts file.
    
    - Added controller for fetching all posts at GET /api/posts
    - Added controller for fetching a single post by ID at GET /api/posts/:postId
    - Added controller for creating a new post at POST /api/posts
    - Added controller for updating an existing post at PUT /api/posts/:postId
    - Added controller for deleting a post by ID at DELETE /api/posts/:postId
    
    Each controller handles request validation, database operations, and error handling.

---
+++ src/controllers/postControllers.ts
@@ -0,0 +1,165 @@
+import { NextFunction, Request, Response } from 'express';
+import { Post } from '../models/postModel';
+import { CustomRequest } from '../middlewares/authenticateMiddleware';
+
+/**
+ * Fetches all posts from the database.
+ * @param { Request } req - Express Request object
+ * @param { Response } res - Express Response object
+ * @param { NextFunction }  next - Express Next function
+ * @returns { Promise<Response> } - Returns a JSON response with all posts or an error message
+ */
+export const GetAllPosts = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // Retrieve all posts from the database
+        const allPosts = await Post.find().populate({path: 'user', select: 'username'}).sort({createdAt: -1});
+
+        // Check if no post exists
+        if (!allPosts.length) {
+            return res.status(404).json({ data: [] });
+        };
+
+        // Return all the retrieved posts
+        return res.status(200).json({data: allPosts});
+    } catch (error) {
+        // Pass the error to the error handling middleware
+        return next(error);
+    };
+};  
+
+/**
+ * Fetches a single post by ID.
+ * @param { Request } req - Express Request object with postId parameter
+ * @param { Response } res - Express Response object
+ * @param { NextFunction } next - Express Next function
+ * @returns { Promise<Response> } - Returns a JSON response with the requested post or an error message
+ */
+export const GetAPost = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // Fetch the post by ID, populating the author's details
+        const post = await Post.findById(req.params.postId)
+            .populate({
+                path: 'user',
+                select: 'username about work'
+            });
+
+        // Check if no post exists with the given post id
+        if (!post) {
+            return res.status(404).json({ data: null });
+        };
+
+        // Populate comments only if the post has comments
+        if (post?.comments && post.comments.length > 0) {
+            await post.populate({path : 'comments'});
+        };
+
+        // Return the retrieved post
+        return res.status(200).json({data: post});
+    } catch (error) {
+        // Pass the error to the error handling middleware
+        return next(error);
+    };
+};
+
+/**
+ * Creates a new post.
+ * @param { Request } req - Express Request object with title, body, and tags
+ * @param { Response } res - Express Response object
+ * @param { NextFunction } next - Express Next function
+ * @returns { Promise<Response> } - Returns a JSON response with the newly created post or an error message
+ */
+export const CreatePost = async (req: CustomRequest, res: Response, next: NextFunction) => {
+    try {
+        // Extract post information and author ID from the request
+        const { title, body, tags } = req.body;
+        const userId = req.user;
+
+        // Create a new post
+        const newPost = await Post.create({
+            title,
+            body,
+            tags,
+            author: userId
+        });
+
+        // Return the new created post along with a success message
+        return res.status(200).json({ message: 'Post created successfully', data: newPost });
+    } catch (error) {
+        // Pass the error to the error handling middleware
+        return next(error);
+    };
+};
+
+/**
+ * Updates an existing post.
+ * @param { Request } req - Express Request object with postId, title, body, and tags
+ * @param { Response } res - Express Response object
+ * @param { NextFunction } next - Express Next function
+ * @returns { Promise<Response> } - Returns a JSON response with the updated post or an error message
+ */
+export const UpdatePost = async (req: CustomRequest, res: Response, next: NextFunction) => {
+    try {
+        // Extract post ID and updated information from the request
+        const postId = req.params.postId;
+        const userId = req.user;
+        const { title, body, tags } = req.body;
+    
+        // Find the post by its ID
+        const existingPost = await Post.findById(postId);
+    
+        // Check if the post exists
+        if (!existingPost) {
+            return res.status(404).json({ message: 'Post not found' });
+        };
+
+        // Check if the authenticated user is the owner of the post
+        if (existingPost.author.toString() !== userId?.toString()) {
+            return res.status(403).json({ message: 'Forbidden. You are not the owner of this post.' });
+        };
+    
+        // Update the post data
+        const UpdatedPost = await Post.findByIdAndUpdate(postId, { title, body, tags }, { new: true } );
+    
+        // Return the updated post along with a success message
+        return res.status(200).json({ message: 'Post updated successfully', data: UpdatedPost });
+    } catch (error) {
+        // Pass the error to the error handling middleware
+        return next(error);
+    };
+};
+
+/**
+ * Deletes a post by its ID.
+ * @param { Request } req - Express Request object with postId
+ * @param { Response } res - Express Response object
+ * @param { NextFunction } next - Express Next function
+ * @returns { Promise<Response> } - Returns a JSON response with success message or an error message
+ */
+export const DeletePost = async (req: CustomRequest, res: Response, next: NextFunction) => {
+    try {
+        // Find the post by its ID
+        const post = await Post.findById(req.params.postId);
+
+        // Ensure the post exists to delete
+        if (!post) {
+            return res.status(404).json({ data: null });
+        };
+        
+        // Extract user id 
+        const userId = req.user;
+
+        // Check if the authenticated user is the owner of the post
+        if (post?.author.toString() !== userId?.toString()) {
+            return res.status(403).json({ message: 'Forbidden. Unauthorized user.' });
+        };
+
+        // Delete the post
+        await Post.findByIdAndDelete(req.params.postId);
+
+        // Return a success message
+        return res.status(204).json({ message: 'Post deleted successfully' });
+    } catch (error) {
+        // Pass the error to the error handling middleware
+        return next(error);
+    };
+};
