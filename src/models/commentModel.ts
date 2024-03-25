ad4b10b631a86163ea49237fd1e40e50e3da375f
Uwancha <sito2alemu@gmail.com>
2024-03-25 21:56:32 +0300

    feat: Add Comment model and interface
    Added the Comment model and corresponding interface to represent comments in the database.

---
+++ src/models/commentModel.ts
@@ -0,0 +1,34 @@
+import { model, Schema, SchemaTypes, Types } from 'mongoose';
+
+// Define Comment interface
+export interface IComment {
+    message: string;
+    author: Types.ObjectId;
+};
+
+// Define the Comment Schema
+const CommentSchema = new Schema<IComment>(
+    {
+        message: {
+            type: String,
+            required: true
+        },
+
+        author: {
+            type: SchemaTypes.ObjectId,
+            ref: 'User',
+            required: true
+        }
+    },
+
+    {
+        // Automatically add createdAt and updatedAt timestamps
+        timestamps: true
+    }
+);
+
+// Create the Comment model using the CommentSchema
+const Comment = model<IComment>('comment', CommentSchema);
+
+// Export the Comment model
+export { Comment };
