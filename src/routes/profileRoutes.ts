d0003dd433059014586a51ec2938bc83f627dd6a
Uwancha <sito2alemu@gmail.com>
2024-03-25 21:28:50 +0300

    feat: Add profile routes Added routes for managing user profiles in the profileRoutes.ts file.
    - Added route for retrieving user profile information by user ID at GET /profile/{userId}
    - Added route for updating user biography by user ID at PUT /api/profile/about/{userId}
    - Added route for updating user avatar by user ID at PUT /api/profile/avatar/{userId}
    
    Each route is documented with parameters, expected responses, and access permissions.

---
+++ src/routes/profileRoutes.ts
@@ -0,0 +1,48 @@
+import { Router } from 'express';
+import { VerifyToken } from '../middlewares/authenticateMiddleware';
+import { GetUserProfile, UpdateUserAvatar, UpdateUserBio } from '../controllers/profileControllers';
+
+const router = Router();
+
+/**
+ * Route for retrieving user profile information by user ID.
+ * @route GET /profile/{userId}
+ * @group Profile - Operations related to user profiles
+ * @param {string} userId.path.required - The ID of the user whose profile information to retrieve.
+ * @returns {object} 200 - User profile data.
+ * @returns {Error} 401 - Error message if the user is not authenticated.
+ * @returns {Error} 403 - Error message if the user is not authorized.
+ * @returns {Error} 404 - Error message if the user is not found.
+ * @access Private
+ */
+router.get('/profile/:userId', VerifyToken, GetUserProfile);
+
+/**
+ * Route for updating user biography by user ID.
+ * @route PUT /api/profile/about/{userId}
+ * @group Profile - Operations related to user profiles
+ * @param {string} userId.path.required - The ID of the user whose biography to update.
+ * @param {string} bio.body.required - The new biography content.
+ * @returns {object} 200 - Success message and the updated user profile.
+ * @returns {Error} 401 - Error message if the user is not authenticated.
+ * @returns {Error} 403 - Error message if the user is not authorized to update the profile.
+ * @returns {Error} 404 - Error message if the user is not found.
+ * @access Private
+ */
+router.put('/profile/about/:userId', VerifyToken, UpdateUserBio);
+
+/**
+ * Route for updating user avatar by user ID.
+ * @route PUT /api/profile/avatar/{userId}
+ * @group Profile - Operations related to user profiles
+ * @param {string} userId.path.required - The ID of the user whose avatar to update.
+ * @param {string} avatar.body.required - The new avatar file path.
+ * @returns {object} 200 - Success message and the updated user profile with new avatar.
+ * @returns {Error} 401 - Error message if the user is not authenticated.
+ * @returns {Error} 403 - Error message if the user is not authorized to update the profile.
+ * @returns {Error} 404 - Error message if the user is not found.
+ * @access Private
+ */
+router.put('/profile/avatar/:userId', VerifyToken, UpdateUserAvatar);
+
+export { router as ProfileRoutes };
